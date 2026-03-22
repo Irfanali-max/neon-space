168924e2d6cacbda76436b9faed0ec5a6161f8b5
Irfanali-max <abug00939@gmail.com>
2026-03-22 17:58:38 +0500

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installed');
-});
-self.addEventListener('fetch', (e) => {
-  // آف لائن کیشنگ کے لیے (ابھی اسے سمپل رکھا ہے تاکہ گیم سمتھ چلے)
-});
