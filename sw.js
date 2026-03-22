cc5190ea0fad158b88d53a1490886234ea73a0fb
Irfanali-max <abug00939@gmail.com>
2026-03-22 17:41:43 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Installed');
+});
+self.addEventListener('fetch', (e) => {
+  // آف لائن کیشنگ کے لیے (ابھی اسے سمپل رکھا ہے تاکہ گیم سمتھ چلے)
+});
